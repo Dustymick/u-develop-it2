396e3cda139ec6ddf6a51a7caff8174033c1587a
Dustin Micklos <damicklo5@gmail.com>
2021-08-04 17:37:06 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const sqlite3 = require("sqlite3").verbose();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+});
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other requests(Not Found) Catch all
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+});
